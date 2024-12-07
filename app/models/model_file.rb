f39fa543ae1fcff6ef7861585f9181ae2c8fa267
Andrii Kuluiev <andreykuluev@gmail.com>
2024-12-07 21:27:50 +0200

    Delete output file from model

--- app/models/model_file.rb
+++ app/models/model_file.rb
@@ -1,11 +1,10 @@
 class ModelFile < ApplicationRecord
-  has_one_attached :input_file
-  has_one_attached :output_file
+  has_one_attached :file
 
   validate :acceptable_file_format
 
   def acceptable_file_format
-    return unless input_file.attached?
+    return unless file.attached?
 
     # Accept only certain formats
     accepted_formats = [ "application/vnd.ms-pki.stl",
@@ -16,8 +15,8 @@ class ModelFile < ApplicationRecord
                         "text/plain",
                         "application/x-tika-ooxml" ]
 
-    unless accepted_formats.include? input_file.content_type
-      errors.add(:input_file, "must be a valid STL, OBJ, AMF, 3MF, or G-code file")
+    unless accepted_formats.include? file.content_type
+      errors.add(:file, "must be a valid STL, OBJ, AMF, 3MF, or G-code file")
     end
   end
 end
