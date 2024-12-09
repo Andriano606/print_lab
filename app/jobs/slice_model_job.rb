a76f0d3c08c5552951d20e99c576dbeebc329786
Andrii Kuluiev <andreykuluev@gmail.com>
2024-12-09 22:07:05 +0200

    Use volume

--- app/jobs/slice_model_job.rb
+++ app/jobs/slice_model_job.rb
@@ -9,28 +9,20 @@ class SliceModelJob < ApplicationJob
 
     sanitized_file_name = sanitize_filename(model_file.file.filename.to_s)
     unique_file_name = "#{SecureRandom.uuid}-#{sanitized_file_name}"
-    origin_file_path = Rails.root.join("tmp", "uploads", unique_file_name)
+    origin_file_path = Pathname.new("/workspace/#{unique_file_name}")
     sliced_file_name = unique_file_name.gsub(File.extname(unique_file_name), ".gcode")
-
-    prepare_directory(origin_file_path.dirname)
 
     create_local_file(origin_file_path, model_file)
 
-    unless convert_to_gcode(origin_file_path.dirname, origin_file_path.basename, sliced_file_name)
+    unless convert_to_gcode(origin_file_path.basename.to_s, sliced_file_name)
       Rails.logger.error "G-code conversion failed for #{origin_file_path}"
     end
-
-    delete_tmp_files(origin_file_path, origin_file_path.dirname.join(sliced_file_name))
   end
 
   private
 
   def sanitize_filename(filename)
     filename.gsub(/[^a-zA-Z0-9_\-\.]/, "_")
-  end
-
-  def prepare_directory(directory_path)
-    FileUtils.mkdir_p(directory_path) unless File.directory?(directory_path)
   end
 
   def create_local_file(origin_file_path, model_file)
@@ -41,11 +33,11 @@ class SliceModelJob < ApplicationJob
     Rails.logger.error "Failed to create local file at #{origin_file_path}: #{e.message}"
   end
 
-  def convert_to_gcode(destination_folder_path, origin_file_name, sliced_file_name)
-    docker_command = build_docker_command(destination_folder_path, origin_file_name, sliced_file_name)
+  def convert_to_gcode(origin_file_name, sliced_file_name)
+    docker_command = build_docker_command(origin_file_name, sliced_file_name)
     Rails.logger.info "Running Docker command: #{docker_command.join(' ')}"
 
-    IO.popen(docker_command, err: [ :child, :out ]) do |io|
+    IO.popen(docker_command, err: [:child, :out]) do |io|
       io.each_line do |line|
         Rails.logger.info line.strip # Log the line for debugging
         handle_progress(line.strip) # Parse progress and handle it
@@ -62,7 +54,6 @@ class SliceModelJob < ApplicationJob
     if line.match?(/^\d+ => /) # Matches lines like "20 => Generating perimeters"
       percentage = line.split("=>").first.strip.to_i
       Rails.logger.info "Slicing progress: #{percentage}%"
-      # Optionally, update a progress tracker or broadcast via ActionCable
       ActionCable.server.broadcast(SLICING_PROGRESS_CHANNEL, { progress: percentage })
     elsif line.include?("Detected print stability issues:")
       Rails.logger.warn "Slicing warning: #{line}"
@@ -73,25 +64,12 @@ class SliceModelJob < ApplicationJob
     end
   end
 
-  def build_docker_command(folder_path, origin_file_name, sliced_file_name)
-    escaped_folder_path = Shellwords.escape(folder_path.to_s)
-    escaped_origin_file = Shellwords.escape(origin_file_name.to_s)
-    escaped_sliced_file = Shellwords.escape(sliced_file_name.to_s)
-
+  def build_docker_command(origin_file_name, sliced_file_name)
     [
-      "docker", "run", "--rm", "-it", "--platform", "linux/amd64",
-      "-v", "#{escaped_folder_path}:/workspace",
+      "docker", "run", "--rm", "-v", "/workspace:/workspace",
       "keyglitch/docker-slic3r-prusa3d", "--export-gcode",
-      "-o", "/workspace/#{escaped_sliced_file}",
-      "/workspace/#{escaped_origin_file}"
+      "-o", "/workspace/#{Shellwords.escape(sliced_file_name)}",
+      "/workspace/#{Shellwords.escape(origin_file_name)}"
     ]
-  end
-
-  def delete_tmp_files(*file_paths)
-    file_paths.each do |file_path|
-      File.delete(file_path) if File.exist?(file_path)
-    rescue => e
-      Rails.logger.error "Failed to delete temporary file #{file_path}: #{e.message}"
-    end
   end
 end
